941790a27c6e80fb7e641b8b5ea82a8745d15529
Brayan  Blas <brayan.blas.egoavil30@gmail.com>
2021-05-12 18:02:26 -0500

    first commit

---
+++ src/test/java/com/tecsup/petclinic/service/EmployeeServiceTest.java
@@ -0,0 +1,14 @@
+package com.tecsup.petclinic.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class EmployeeServiceTest {
+
+}
